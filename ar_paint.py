dc9f89e99540480e197991896eae0704f25bbf2e
Roberto Figueiredo <robertof@ua.pt>
2022-11-02 21:50:16 +0000

    add drawing feature

--- ar_paint.py
+++ ar_paint.py
@@ -13,16 +13,13 @@
 
 ################## Library ###################
 import argparse
-from nis import cat
-from click import option
 import cv2
-from exceptiongroup import catch
-from matplotlib import image
+from cv2 import cvtColor
+from matplotlib.pyplot import draw
 import numpy as np
 import json
-import math
 from copy import deepcopy
-from time import ctime, sleep
+from time import ctime
 from pprint import pprint
 ##############################################
 
@@ -39,7 +36,7 @@ def parameters(key, options):
         print('Change pencil color to red')
         options['pencil_color'] = (0,0,250)
 
-    if key == ord('v'): #mudar a cor do lápis para verde
+    if key == ord('g'): #mudar a cor do lápis para verde
         print('Change pencil color to green')
         options['pencil_color'] = (0,255,0)
 
@@ -76,21 +73,34 @@ def findObject(img, mask, options):
                 cv2.circle(img, (int(x+w/2), int(y+h/2)), 5, (0, 0, 255), -1) #cv2.circle(image, center_coordinates, radius, color, thickness)
                 options['xs'].append(int(x+w/2))
                 options['ys'].append(int(y+h/2))
+
+def compare_images(img1, img2):
+    #check similarity between images
+    s = cv2.subtract(img2, img1)
+    #count black pixels in image s
+    s = cv2.cvtColor(s, cv2.COLOR_BGR2GRAY)
+    num_black_pix = cv2.countNonZero(s)  
+    print('Number of black pixels:', num_black_pix)
+    total_pixels = s.shape[0]*s.shape[1]
+    percentage_correct = 100-((num_black_pix*100)/total_pixels)
+    return percentage_correct
+
 def main():
     mode = "regular"
+    drawing = False
     parser = argparse.ArgumentParser(description='Definition of test mode')
     parser.add_argument('-j', '--json', type=str, required=True, help='Full path to json file.')
     parser.add_argument('-usp', '--use_shake_prevention', type=str, required=False, help='Runs the snake prevention code.')
     parser.add_argument('-f', '--use_feed',  required=False, help='Uses the camera feed as canvas.')
-
-    #args = vars(parser.parse_args())
+    parser.add_argument('-d', '--drawing', required=False, help='Use a drawing as canvas.')
+    args = vars(parser.parse_args())
 
     #Abre o ficheiro json
     #E retira os valores de max e min definidos no color_segmenter.py e coloca-os numa lista
     min=[]
     max=[]
 
-    f=open('limits.json')
+    f=open(args['json'])
     data=json.load(f)
     pprint(data)
     for i in data['limits'].values():
@@ -99,21 +109,29 @@ def main():
     f.close() #fechar o ficheiro
 
     lower = np.array(min)
-    upper = np.array(max)  
-
+    upper = np.array(max)
    
     cap = cv2.VideoCapture(0)
     _, img = cap.read()
-    paint_w = np.zeros((img.shape[0], img.shape[1],3),dtype = np.uint8) #criação da "tela" com o mesmo tamanho que as outras janelas->(500,700) corresponde ao tamanho, 3 é o canal e uint8 é o tipo
+    paint_w = np.zeros((img.shape[0], img.shape[1],3),dtype = np.uint8) 
     paint_w.fill(255) #correponde à cor de background, 255 corresponde à cor branca
     options = {'paint_wind':  paint_w,'xs': [], 'ys':[], 'pencil_color':(0,255,0), 'pencil_size':3}  
     
+    if args['drawing']:
+        image_color = cv2.imread('color.png', 1)
+        image_bw = cv2.imread('bw.png',1)
+        image_color = cv2.resize(image_color, (img.shape[1], img.shape[0]), interpolation = cv2.INTER_AREA)
+        image_bw = cv2.resize(image_bw, (img.shape[1], img.shape[0]), interpolation = cv2.INTER_AREA)
+        options['paint_wind'] = image_bw
+        drawing = True
+        initial_percentage = compare_images(image_color, image_bw)
+
     cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Original', img.shape[1], img.shape[0])
     cv2.namedWindow('Mask',cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Mask', img.shape[1], img.shape[0])
-    cv2.namedWindow('paint',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('paint', img.shape[1], img.shape[0])
+    cv2.namedWindow('Paint',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Paint', img.shape[1], img.shape[0])
 
     def paint_circle(window, center, coordinates):
         #draw an ellipse
@@ -121,23 +139,10 @@ def main():
     def paint_rectangle(window, center, radius):
         #draw an ellipse
         cv2.rectangle(window, center, radius, options['pencil_color'], options['pencil_size'])
-    def draw_on_feed(options):
-        if (len(options['xs'])>2) & (len(options['ys'])>2):#Para garantir que as litas têm pelo menos dois elementos
-            xs = iter(options['xs'])
-            ys = iter(options['ys'])
-            last_used = (next(xs), next(ys))
-            try:
-                for x, y in zip(xs, ys):
-                    cv2.line(options['paint_wind'], (last_used[0], last_used[1]), (x, y), options['pencil_color'], options['pencil_size'])
-                    last_used = (x, y)
-            except Exception:
-                pass
     key_presses = []
     while True:
         _, img = cap.read()
-        #array with max length of 10
         key_presses = key_presses[-2:]
-        #options['paint_wind'] = paint_w
         mask = cv2.inRange(img, lower, upper)
         findObject(img, mask, options)
         press_key = cv2.waitKey(30)
@@ -153,6 +158,10 @@ def main():
                 center = (options['xs'][-1], options['ys'][-1])
             mode = 'rectangle'
         elif press_key == ord('q'): #sai do programa 
+            if drawing:
+                percentage = compare_images(image_color, options['paint_wind'])
+                percentage -= initial_percentage
+                print("Percentage of the drawing correct: {0:.0f}%".format(percentage))
             break
         else:
             mode = "regular"
@@ -177,10 +186,12 @@ def main():
         
         cv2.imshow('Original', img)
         cv2.imshow('Mask', mask)
-        cv2.imshow('paint', options['paint_wind'])
-        s =  cv2.bitwise_and(img, options['paint_wind'])
-        cv2.imshow('s',s)
+        if args['use_feed']:
+            feed =  cv2.bitwise_and(img, options['paint_wind'])
+            cv2.imshow('Paint',feed)
+        else:
+            cv2.imshow('Paint', options['paint_wind'])
     cv2.destroyAllWindows()
-    
+
 if __name__ == '__main__':
     main()
